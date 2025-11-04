ed1f8df62a94d7446add3c1cde338d43b07b3d9d
josemanuelborja <jmborja11296@liceo.edu.ph>
2025-11-04 20:58:14 +0800

    feature: completed dashboard feature

--- src/main/java/com/example/dentra/DashboardController.java
+++ src/main/java/com/example/dentra/DashboardController.java
@@ -1,4 +1,28 @@
 package com.example.dentra;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardController {
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private void handleLogOutClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        String styleCss = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(styleCss);
+
+        Stage stage = (Stage) logoutBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/dentra/MainApplication.java
+++ src/main/java/com/example/dentra/MainApplication.java
@@ -3,7 +3,7 @@ package com.example.dentra;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Scene;  
 import javafx.stage.Stage;
 
 import java.io.IOException;
